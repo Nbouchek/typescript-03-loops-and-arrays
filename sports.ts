f3b18d79f79d54d74e3847bf5347cb39c7b5adef
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-01 13:42:09 +0000

    Loops - simplified loop.

--- sports.ts
+++ sports.ts
@@ -3,3 +3,9 @@ let sportsOne: string[] = ["Golf", "Cricket", "Fencing", "Tennis", "Swiming"];
 for (let index = 0; index < sportsOne.length; index++) {
   console.log(sportsOne[index]);
 }
+
+// simplified for loop
+
+for (const iterator of sportsOne) {
+  console.log(iterator);
+}
